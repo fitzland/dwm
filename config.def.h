ba086174ac5615afa3b54dfc8bf67303473f1213
fitzland <f1tzl4nd@gmail.com>
2022-03-19 17:04:02 -0500

    increase font size

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=11" };
+static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -36,6 +36,7 @@ static const Rule rules[] = {
 	{ "Google-chrome",     NULL,       NULL,       1 << 5,            0,           1 },
 	{ "Microsoft-edge",     NULL,       NULL,       1 << 5,            0,           1 },
 	{ "KeePassXC",     NULL,       NULL,       1 << 7,            0,           -1 },
+	{ "Spotify",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
